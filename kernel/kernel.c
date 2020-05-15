70a2d19e34271a0c38210f816115aa6df69451e0
splichte <splichte@gmail.com>
2020-05-15 00:55:31 -0400

    Finish keyboard mapping

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -160,11 +160,43 @@ INT(46);
 INT(47);
 
 
-uint8_t scancodes[255] = {
-    0, '`', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    '0', '-', '=', 0, 0, 'q', 'w', 'e', 'r', 
-    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 
+/* TODO: move this to separate "keyboard" file. */
+/* standard ASCII symbols */
+#define BACKSPACE 8
+#define TAB 9
+#define LINE_FEED 10
+#define SHIFT_OUT 14
+#define SHIFT_IN 15
+#define ESC 27 
+
+
+// ones I defined
+// TODO: make an enum
+// I'm just doing these for testing...
+#define CAPS_LOCK 256
+#define CTRL 259
+#define ALT 260
+#define MISSING 0
+
+
+/* I figured these out by using print_byte on the incoming keys for the 
+ * keyboard. */
+/* Most of the keys we need (the "down" presses) start at the beginning of the 
+ * scancode range. */
+/* I use extended number range to represent things like 
+ * caps lock, etc. */
+uint16_t scancodes[255] = {
+    MISSING, CAPS_LOCK, '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '0', '-', '=', BACKSPACE, TAB, 'q', 'w', 'e', 'r', 
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', LINE_FEED, CTRL,
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 
+    '`', SHIFT_IN, MISSING, 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', 
+    '.', '/', SHIFT_IN, MISSING, ALT, ' ', ESC,
 };
+
+scancodes[170] = SHIFT_OUT;
+scancodes[182] = SHIFT_OUT;
+
 
 // definitions for the Interrupt Descriptor Table (IDT)
 // see [2]. the table is composed of "gates" 
@@ -239,7 +271,9 @@ void kbd_handler(struct interrupt_frame *frame) {
         uint8_t buf[2];
         buf[0] = scancodes[key];
         buf[1] = '\0';
-        print(buf);
+        if (buf[0] != MISSING) {
+            print(buf);
+        }
     }
 
     port_byte_out(PIC1A, PIC_EOI);
